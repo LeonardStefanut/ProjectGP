c54f3bcf00411fc37e789d7a03994ab46fe3f46f
LeonardStefanut <steleonard2005@gmail.com>
2025-12-29 18:04:16 +0200

    wasd

--- Camera.cpp
+++ Camera.cpp
@@ -8,6 +8,9 @@ namespace gps {
         this->cameraPosition = cameraPosition;
         this->cameraTarget = cameraTarget;
         this->cameraUpDirection = cameraUp;
+
+        this->cameraFrontDirection = glm::normalize(cameraTarget - cameraPosition);
+        this->cameraRightDirection = glm::normalize(glm::cross(cameraFrontDirection, cameraUpDirection));
         
     }
 
@@ -15,12 +18,26 @@ namespace gps {
     glm::mat4 Camera::getViewMatrix() {
         //TODO
 
-        return glm::lookAt(cameraPosition, cameraTarget, this->cameraUpDirection);
+        return glm::lookAt(cameraPosition, cameraPosition + cameraFrontDirection, cameraUpDirection);
     }
 
     //update the camera internal parameters following a camera move event
     void Camera::move(MOVE_DIRECTION direction, float speed) {
         //TODO
+        switch (direction) {
+        case MOVE_FORWARD:
+            cameraPosition += cameraFrontDirection* speed;
+            break;
+        case MOVE_BACKWARD:
+            cameraPosition -= cameraFrontDirection* speed;
+            break;
+        case MOVE_LEFT:
+            cameraPosition -= cameraRightDirection * speed;
+            break;
+        case MOVE_RIGHT:
+            cameraPosition += cameraRightDirection *speed;
+            break;
+        }
     }
 
     //update the camera internal parameters following a camera rotate event
@@ -28,5 +45,13 @@ namespace gps {
     //pitch - camera rotation around the x axis
     void Camera::rotate(float pitch, float yaw) {
         //TODO
+        glm::mat4 rotationY = glm::rotate(glm::mat4(1.0f), glm::radians(yaw), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        glm::mat4 rotationX = glm::rotate(glm::mat4(1.0f), glm::radians(pitch), cameraRightDirection);
+
+        cameraFrontDirection = glm::vec3(rotationY * rotationX * glm::vec4(cameraFrontDirection, 0.0f));
+        cameraFrontDirection = glm::normalize(cameraFrontDirection);
+
+        cameraRightDirection = glm::normalize(glm::cross(cameraFrontDirection, glm::vec3(0.0f, 1.0f, 0.0f)));
     }
 }
